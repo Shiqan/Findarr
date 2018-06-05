0cc625b48b10ef18889bb08a61601bf22de4a3b7
Shiqan <ferron.saan@live.nl>
2018-06-05 22:21:49 +0200

    Add EJS view engine

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from 'express';
+const engine = require('ejs-locals');
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 
 const schema = require('./schema.js');
 
 const app = express();
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('index');
+  });
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }));
